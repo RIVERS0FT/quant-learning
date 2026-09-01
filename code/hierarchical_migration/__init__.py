7cc783c6e99a21b563cde50b7d54e6310635caea
RIVERSOFT <105160096+RIVERS0FT@users.noreply.github.com>
2026-09-01 19:20:26 +0800

    feat: export market data pipeline and taxonomy

--- code/hierarchical_migration/__init__.py
+++ code/hierarchical_migration/__init__.py
@@ -1,5 +1,29 @@
 from .config import CORE_FEATURES, HierarchicalConfig
+from .data_pipeline import (
+    MarketDataConfig,
+    build_market_dataset,
+    build_security_master,
+)
 from .engine import run_hierarchy
 from .features import latest_snapshot, prepare_panel
+from .taxonomy import (
+    CANONICAL_L1,
+    apply_unified_taxonomy,
+    map_to_unified,
+    taxonomy_coverage,
+)
 
-__all__ = ["CORE_FEATURES", "HierarchicalConfig", "prepare_panel", "latest_snapshot", "run_hierarchy"]
+__all__ = [
+    "CORE_FEATURES",
+    "HierarchicalConfig",
+    "MarketDataConfig",
+    "prepare_panel",
+    "latest_snapshot",
+    "run_hierarchy",
+    "build_security_master",
+    "build_market_dataset",
+    "CANONICAL_L1",
+    "map_to_unified",
+    "apply_unified_taxonomy",
+    "taxonomy_coverage",
+]
